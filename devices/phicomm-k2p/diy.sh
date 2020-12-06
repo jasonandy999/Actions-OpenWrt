29bb137bc994e5950e51fb41042de77e8ca46ef4
jasonandy999 <59506376+jasonandy999@users.noreply.github.com>
2020-12-07 02:51:18 +0800

    Update diy.sh

--- devices/phicomm-k2p/diy.sh
+++ devices/phicomm-k2p/diy.sh
@@ -6,3 +6,7 @@ echo -e "\q" | svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ra
 sed -i 's/O2/Os/g' include/target.mk
 
 sed -i 's/PKG_VERSION:=1/PKG_VERSION:=2/' package/feeds/custom/luci-app-ssr-plus/Makefile
+git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
